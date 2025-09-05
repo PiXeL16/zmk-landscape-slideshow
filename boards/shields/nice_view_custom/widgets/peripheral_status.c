59a6bb961b6f78e90a463f4fdca8623c1cb5ad5c
Chris J <cjimenez16@gmail.com>
2025-09-05 12:03:51 +0200

    Add new images and update peripheral status declarations to include images 46-57

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -68,6 +68,18 @@ LV_IMG_DECLARE(image42);
 LV_IMG_DECLARE(image43);
 LV_IMG_DECLARE(image44);
 LV_IMG_DECLARE(image45);
+LV_IMG_DECLARE(image46);
+LV_IMG_DECLARE(image47);
+LV_IMG_DECLARE(image48);
+LV_IMG_DECLARE(image49);
+LV_IMG_DECLARE(image50);
+LV_IMG_DECLARE(image51);
+LV_IMG_DECLARE(image52);
+LV_IMG_DECLARE(image53);
+LV_IMG_DECLARE(image54);
+LV_IMG_DECLARE(image55);
+LV_IMG_DECLARE(image56);
+LV_IMG_DECLARE(image57);
 
 const lv_img_dsc_t *anim_imgs[] = {
     &image1,
@@ -115,7 +127,24 @@ const lv_img_dsc_t *anim_imgs[] = {
     &image43,
     &image44,
     &image45,
+    &image46,
+    &image47,
+    &image48,
+    &image49,
+    &image50,
+    &image51,
+    &image52,
+    &image53,
+    &image54,
+    &image55,
+    &image56,
+    &image57,
 };
+
+
+
+
+
 
 
 
